b8444cb5df7138ffa523e74e0a1a3cb116a70660
soohyun__kim <tngusrla0812@gmail.com>
2024-01-26 16:51:33 +0900

    [20240126] ing~

--- Assets/Scripts/AchiveManager.cs
+++ Assets/Scripts/AchiveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,5 +8,18 @@ public class AchiveManager : MonoBehaviour
     public GameObject[] lockCharacters;
     public GameObject[] unlockCharacters;
 
-    
+    enum Achive { UnlockPotato, UnlockBean }
+
+    Achive[] achives;
+
+    void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));    
+    }
+
+    void Start()
+    {
+        
+    }
+
 }
